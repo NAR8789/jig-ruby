6cda065c21a547873f15675188c41b46b8c5657f
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-01-07 04:34:29 -0800

    initial specs

--- spec/qig_spec.rb
+++ spec/qig_spec.rb
@@ -5,7 +5,37 @@ RSpec.describe Qig do
     expect(Qig::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#qig', :aggregate_failures do
+    it 'handles trivial caess' do
+      expect(Qig.qig({})).to eq({})
+      expect(Qig.qig([])).to eq([])
+    end
+
+    it 'handles simple hash cases' do
+      expect(Qig.qig({a: 1, b: 2}, :a)).to eq(1)
+      expect(Qig.qig({a: 1, b: 2}, :b)).to eq(2)
+      expect(Qig.qig({a: 1, b: 2}, :c)).to eq(nil)
+      expect(Qig.qig({a: 1, b: 2}, :c, :d)).to eq(nil)
+    end
+
+    it 'handles simple array cases' do
+      expect(Qig.qig([1, 2], 0)).to eq(1)
+      expect(Qig.qig([1, 2], 1)).to eq(2)
+      expect(Qig.qig([1, 2], 2)).to eq(nil)
+      expect(Qig.qig([1, 2], 2, 0)).to eq(nil)
+      expect(Qig.qig([[1, 2], [3, 4]], 0)).to eq([1, 2])
+    end
+
+    it 'treats the first level of [] as array descent' do
+      expect(Qig.qig([[1, 2], [3, 4]], [])).to eq([[1, 2], [3, 4]])
+      expect(Qig.qig([[1, 2], [3, 4]], [], 0)).to eq([1, 3])
+      expect(Qig.qig([[1, 2], [3, 4]], [], 1)).to eq([2, 4])
+      expect(Qig.qig([[1, 2], [3, 4]], [], 2)).to eq([nil, nil])
+      expect(Qig.qig([[1, 2], [3, 4]], [], 2, 0)).to eq([nil, nil])
+    end
+
+    it 'treats the second level of [] as flattening' do
+      expect(Qig.qig([[1, 2], [3, 4]], [], [])).to eq([1, 2, 3, 4])
+    end
   end
 end
