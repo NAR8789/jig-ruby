fffab76b9b3887f93e5e4e5eba97016fbb5e745e
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-01-07 06:37:45 -0800

    initial implementation

--- lib/qig.rb
+++ lib/qig.rb
@@ -3,6 +3,38 @@
 require_relative 'qig/version'
 
 module Qig
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.qig(*args)
+    qig_apply(*args)
+  end
+
+  def self.qig_apply(unit, *path)
+    return unit if unit.nil?
+
+    head, *rest = path
+    case head
+    when nil
+      unit
+    when []
+      qig_map(unit, *rest)
+      # hmm... what should happen if I [] on something not an array?
+    else
+      qig_apply(unit[head], *rest)
+    end
+  end
+
+  def self.qig_map(collection, *path)
+    return collection if collection.empty?
+
+    head, *rest = path
+    case head
+    when nil
+      collection
+    when []
+      qig_map(collection.flatten(1), *rest)
+      # not sure this is quite jq-compliant... [] refuses to iterate over atoms, but flatten will just preserve them.
+      # maybe more in the spirit of `dig` though?
+    else
+      qig_map(collection.map { |e| e.nil? ? e : e[head] }, *rest)
+    end
+  end
 end
