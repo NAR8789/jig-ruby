d3b222786332550e4442cd749898f6b2ee39a8c4
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-01-08 13:05:27 -0800

    WIP first attempts

--- spec/qig_spec.rb
+++ spec/qig_spec.rb
@@ -23,19 +23,80 @@ RSpec.describe Qig do
       expect(Qig.qig([1, 2], 1)).to eq(2)
       expect(Qig.qig([1, 2], 2)).to eq(nil)
       expect(Qig.qig([1, 2], 2, 0)).to eq(nil)
-      expect(Qig.qig([[1, 2], [3, 4]], 0)).to eq([1, 2])
+
+      # this last example is interesting... what if we want to 
     end
 
-    it 'treats the first level of [] as array descent' do
-      expect(Qig.qig([[1, 2], [3, 4]], [])).to eq([[1, 2], [3, 4]])
-      expect(Qig.qig([[1, 2], [3, 4]], [], 0)).to eq([1, 3])
-      expect(Qig.qig([[1, 2], [3, 4]], [], 1)).to eq([2, 4])
-      expect(Qig.qig([[1, 2], [3, 4]], [], 2)).to eq([nil, nil])
-      expect(Qig.qig([[1, 2], [3, 4]], [], 2, 0)).to eq([nil, nil])
+    describe '[]' do
+      it 'treats the first level of [] as array descent' do
+        expect(Qig.qig([[1, 2], [3, 4]], [])).to eq([[1, 2], [3, 4]])
+        expect(Qig.qig([[1, 2], [3, 4]], [], 0)).to eq([1, 3])
+        expect(Qig.qig([[1, 2], [3, 4]], [], 1)).to eq([2, 4])
+        expect(Qig.qig([[1, 2], [3, 4]], [], 2)).to eq([nil, nil])
+        expect(Qig.qig([[1, 2], [3, 4]], [], 2, 0)).to eq([nil, nil])
+      end
+
+      it 'unboxes top level collections' do
+        expect(Qig.collection_qig([[1, 2], [3, 4]], [])).to eq([1, 2, 3, 4])
+      end
+
+      it 'preserves atoms' do
+        expect(Qig.collection_qig([1, 2], [])).to eq([1, 2])
+        expect(Qig.collection_qig([[1, 2], 3, [4, 5]], [])).to eq([1, 2, 3, 4, 5])
+      end
+      
+      # But there's some ambiguity here: shouldn't the outer enclosing 
+      # 
+
+      it 'treats the second level of [] as flattening' do
+        # in jq, [] unboxes top-level arrays
+
+        expect(Qig.collection_qig([[1, 2], [3, 4]], [])).to eq([1, 2, 3, 4])
+
+        # but in qig, this conflicts conceptually with the simple array handling
+        #
+        # Should Qig.qig([[1, 2], [3, 4]], []) operate on the outermost array, or on the inner arrays?
+        # - In jq, this would operate at the top-level:
+        #   unwrap the top-level array and add its elements to the stream, producing [1, 2] [3, 4]
+        # - This is consistent with jq's indexing behavior in the simple array case:
+
+
+        #
+        # We can emulate a stream by wrapping the results in an array:
+        
+        expect(Qig.qig [[1, 2], [3, 4]]).to eq([[1, 2], [3, 4]])
+
+        # but isn't this just the input?
+        #
+        # Yes, but Qig's internal state has changed. Since the outer array is now the "stream", 
+
+        expect(Qig.qig([[1, 2], [3, 4]], [], [])).to eq([1, 2, 3, 4])
+      end
+
+      # [] is conceptually simple: it unboxes collections and dumps their contents into jq's stream.
+      # It's very simlar to a flatten, except jq will raise an exception if we try to unbox an atom.
+      # TODO: look at how jq formally defines this and rewrite for consistency.
+      #
+      # In qig, [] _is exactly_ flatten: it unboxes top-level collections, and preserves atoms
+      #
+      # But, the prior tests show qig treating its input as a single atom rather than a stream.
+      # If we're following jq semantics, then given an array shouldn't Qig try to unbox the top-level array?
+      # That is to say...
+      #
+      # Qig.qig([[1, 2], [3, 4]], [])) => [1, 2], [3, 4] # what would this even mean? Are we suddenly writing perl?
+      #
+      # Qig solves this as follows:
+      # - initial input is treated as a unit (or a stream of one), and it tries to return a unit
+      # - if we try to _unbox_ the initial unit, Qig drops down a level, treating that top-level collection as the stream.
+
     end
 
-    it 'treats the second level of [] as flattening' do
-      expect(Qig.qig([[1, 2], [3, 4]], [], [])).to eq([1, 2, 3, 4])
+    context 'when I [] something that is not an array'
+      it 'preserves the item (like flatten)' do
+
+      end
+
+      xit 'throws an error (like jq)'
     end
   end
 end
