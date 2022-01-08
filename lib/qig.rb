cd8391d3ed48ee973cd3938607c1781c180da398
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-01-08 06:42:46 -0800

    unit_ and collection_ as opposed to _apply and _map

--- lib/qig.rb
+++ lib/qig.rb
@@ -4,37 +4,37 @@ require_relative 'qig/version'
 
 module Qig
   def self.qig(*args)
-    qig_apply(*args)
+    unit_qig(*args)
   end
 
-  def self.qig_apply(unit, *path)
-    return unit if unit.nil?
+  def self.unit_qig(subject, *path)
+    return subject if subject.nil?
 
     head, *rest = path
     case head
     when nil
-      unit
+      subject
     when []
-      qig_map(unit, *rest)
+      collection_qig(subject, *rest)
       # hmm... what should happen if I [] on something not an array?
     else
-      qig_apply(unit[head], *rest)
+      unit_qig(subject[head], *rest)
     end
   end
 
-  def self.qig_map(collection, *path)
-    return collection if collection.empty?
+  def self.collection_qig(subjects, *path)
+    return subjects if subjects.empty?
 
     head, *rest = path
     case head
     when nil
-      collection
+      subjects
     when []
-      qig_map(collection.flatten(1), *rest)
+      collection_qig(subjects.flatten(1), *rest)
       # not sure this is quite jq-compliant... [] refuses to iterate over atoms, but flatten will just preserve them.
       # maybe more in the spirit of `dig` though?
     else
-      qig_map(collection.map { |e| e.nil? ? e : e[head] }, *rest)
+      collection_qig(subjects.map { |e| e.nil? ? e : e[head] }, *rest)
     end
   end
 end
