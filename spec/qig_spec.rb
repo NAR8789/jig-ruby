1558a38726eb186a6fe70e42574be370b910db99
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-01-19 01:56:37 -0800

    WIP dig conformance testing

--- spec/qig_spec.rb
+++ spec/qig_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe Qig, :aggregate_failures do
   # That is to say...
 
   context 'it behaves like dig' do
-    it 'handles dig documentation examples' # TODO: rephrase this
-
     it 'handles simple hash cases' do
       expect(Qig.qig({ a: 1, b: 2 }, :a)).to eq(1)
       expect(Qig.qig({ a: 1, b: 2 }, :b)).to eq(2)
@@ -31,6 +29,63 @@ RSpec.describe Qig, :aggregate_failures do
     it 'handles simple OpenStruct cases' do
       require 'ostruct'
       expect(Qig.qig(OpenStruct.new(a: 1, b: 2), :a)).to eq 1 # rubocop:disable Style/OpenStructUse
+    end
+
+    context 'dig documentation examples' do
+      context 'Dig Methods https://ruby-doc.org/core-3.1.0/doc/dig_methods_rdoc.html' do
+        subject(:item) do
+          {
+            id: '0001',
+            type: 'donut',
+            name: 'Cake',
+            ppu: 0.55,
+            batters: {
+              batter: [
+                { id: '1001', type: 'Regular' },
+                { id: '1002', type: 'Chocolate' },
+                { id: '1003', type: 'Blueberry' },
+                { id: '1004', type: "Devil's Food" }
+              ]
+            },
+            topping: [
+              { id: '5001', type: 'None' },
+              { id: '5002', type: 'Glazed' },
+              { id: '5005', type: 'Sugar' },
+              { id: '5007', type: 'Powdered Sugar' },
+              { id: '5006', type: 'Chocolate with Sprinkles' },
+              { id: '5003', type: 'Chocolate' },
+              { id: '5004', type: 'Maple' }
+            ]
+          }
+        end
+
+        it 'satisfies the dig documentation examples' do
+          expect(Qig.qig(item, :batters, :batter, 1, :type)).to eq 'Chocolate'
+          expect(Qig.qig(item, :batters, :BATTER, 1, :type)).to eq nil
+        end
+
+        it 'can additionally fetch _all_ the batters' do
+          expect(Qig.qig(item, :batters, :batter, [], :type))
+            .to eq(['Regular', 'Chocolate', 'Blueberry', "Devil's Food"])
+        end
+
+        it 'unexpectedly can qig into integers' do
+          # dig specifies "A dig method raises an exception if any receiver does not respond to #dig"
+
+          # qig mostly just relies on receivers to respond to `[]`. Accordingly, our error is different
+          expect { Qig.qig({ foo: 1 }, :foo, :bar) }.to raise_error(TypeError, 'no implicit conversion of Symbol into Integer')
+          expect { Qig.qig(1, :bar) }.to raise_error(TypeError, 'no implicit conversion of Symbol into Integer')
+
+          # So wait, this will work if we use a numeric index?
+          expect(Qig.qig(1, 0)).to eq(1)
+          expect(Qig.qig(1, 1)).to eq(0)
+          expect(Qig.qig(0x1234, 4...12).to_s(16)).to eq('23')
+
+          # Turns out [] is defined in a lot more places than I'd expect. Integer#[] indexes into the bitstring
+          expect(0x1234.to_s(2)).to eq('1001000110100')
+          expect(0x1234[]).to_s()).to eq('23')
+        end
+      end
     end
   end
 
@@ -152,7 +207,10 @@ RSpec.describe Qig, :aggregate_failures do
     # - switching to collection-context is short, explicit, and sticks to jq semantics.
 
     context 'qig always starts in unit context' do
-      it 'treats initial input as a unit, even when that input is an array'
+      it 'treats initial input as a unit, even when that input is an array' do
+        expect(Qig.qig([[1, 2], [3, 4]], 0)).to eq([1, 2])
+        expect(Qig.qig([[1, 2], [3, 4]], 0)).to eq([1, 2])
+      end
     end
 
     context 'qig switches to collection context upon first invocation of []' do
